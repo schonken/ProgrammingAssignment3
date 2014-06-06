e23c5525396ac6a99bcc634bc1fd420d16d24dc6
Francois Schonken <francois.schonken@gmail.com>
2014-06-06 12:45:04 +1000

    Init Repo

---
+++ scratch.R
@@ -0,0 +1,5 @@
+## 1 Plot the 30-day mortality rates for heart attack
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+head(outcome)
+outcome[, 11] <- as.numeric(outcome[, 11])
+hist(outcome[, 11])
